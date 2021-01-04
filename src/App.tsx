c39851846ecf319928e13d25084e76645dee7c34
Keith Billings <heyitsbo91@gmail.com>
2021-01-04 15:16:01 -0700

    add comments

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,10 @@ import Todo from "./todo.model";
 const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  const todoAddHandler = (text: string) => {   // function that is called when button is pressed. Here to add a new todo item to the todo list array
-    setTodos(previousTodos => [ // passing a function into the set state, taking the old array of todos as the argument
+  const todoAddHandler = (text: string) => {
+    // function that is called when button is pressed. Here to add a new todo item to the todo list array
+    setTodos((previousTodos) => [
+      // passing a function into the set state, taking the old array of todos as the argument
       ...previousTodos, // old array
       { id: Math.random().toString(), text: text }, // and adding on the new todo list item
     ]);
@@ -20,8 +22,8 @@ const App: React.FC = () => {
       <TodoList items={todos} />
     </div>
   );
-}
+};
 export default App;
 
-// do not use React.FC
+// in the future, do not use React.FC
 // instead use JSX.Element
